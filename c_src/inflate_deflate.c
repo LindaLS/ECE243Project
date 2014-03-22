feb09077a23c5111d83a878c0831e45ac2c08caf
Linda <llinda13579@gmail.com>
2014-03-22 11:26:05 -0400

    inflate/deflate algorithm in c

--- c_src/inflate_deflate.c
+++ c_src/inflate_deflate.c
@@ -1,67 +1,169 @@
-#define NUM_NODES 27
+#define NUM_NODES 52
 #define NUM_CHAR 27
 #define MAX_DEPTH 11
-
-
-Node node[NUM_NODES];
-char convert_table { "00000", //a
-					 "0001100", //b
-					 "011111", //c
-					 "000011", //d
-					 "0010", //e
-					 "010111", //f
-					 "0001111", //g
-					 "00110", //h
-					 "00111", //i
-					 "0101100011", //j
-					 "01011001", //k
-					 "000010", //l
-					 "010101", //m
-					 "01101", //n
-					 "00010", //o
-					 "0001101", //p
-					 "0101100001", //q
-					 "01110", //r
-					 "01100", //s
-					 "0100", //t
-					 "011110", //u
-					 "0101101", //v
-					 "010100", //w
-					 "0101100010", //x
-					 "0001110", //y
-					 "0101100000", //z
-					 "1", //' '
- };
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Node
 {
-	int value;
+	char value;
 	struct Node *left;
 	struct Node *right;
 } Node;
 
+Node node[] = {
+			  { 0,   &node[1],  &node[52]}, //node 0
+			  { 0,   &node[2],  &node[23]}, //node 1 
+			  { 0,   &node[3],  &node[18]}, //node 2
+			  { 0,   &node[4],   &node[9]}, //node 3
+			  { 0,   &node[5],   &node[6]}, //node 4
+			  { 'a', NULL,           NULL}, //node 5
+			  { 0,   &node[7],   &node[8]}, //node 6
+			  { 'l', NULL,           NULL}, //node 7
+			  { 'd', NULL,           NULL}, //node 8
+			  { 0,   &node[10], &node[11]}, //node 9
+			  { 'o', NULL,           NULL}, //node 10
+			  { 0,   &node[12], &node[15]}, //node 11
+			  { 0,   &node[13], &node[14]}, //node 12
+			  { 'b', NULL,           NULL}, //node 13
+			  { 'p', NULL,           NULL}, //node 14
+			  { 0,   &node[16], &node[17]}, //node 15
+			  { 'y', NULL,           NULL}, //node 16
+			  { 'g', NULL,           NULL}, //node 17
+			  { 0,   &node[19], &node[20]}, //node 18
+			  { 'e', NULL,           NULL}, //node 19
+			  { 0,   &node[21], &node[22]}, //node 20
+			  { 'h', NULL,           NULL}, //node 21
+			  { 'i', NULL,           NULL}, //node 22
+			  { 0,   &node[24], &node[43]}, //node 23
+			  { 0,   &node[25], &node[26]}, //node 24
+			  { 't', NULL,           NULL}, //node 25
+			  { 0,   &node[27], &node[30]}, //node 26
+			  { 0,   &node[28], &node[29]}, //node 27
+			  { 'w', NULL,           NULL}, //node 28
+			  { 'm', NULL,           NULL}, //node 29
+			  { 0,   &node[31], &node[42]}, //node 30
+			  { 0,   &node[32], &node[41]}, //node 31
+			  { 0,   &node[33], &node[40]}, //node 32
+			  { 0,   &node[34], &node[37]}, //node 33
+			  { 0,   &node[35], &node[36]}, //node 34
+			  { 'z', NULL,           NULL}, //node 35
+			  { 'q', NULL,           NULL}, //node 36
+			  { 0,   &node[38], &node[39]}, //node 37
+			  { 'x', NULL,           NULL}, //node 38
+			  { 'j', NULL,           NULL}, //node 39
+			  { 'k', NULL,           NULL}, //node 40
+			  { 'v', NULL,           NULL}, //node 41
+			  { 'f', NULL,           NULL}, //node 42
+			  { 0,   &node[44], &node[47]}, //node 43
+			  { 0,   &node[45], &node[46]}, //node 44
+			  { 's', NULL,           NULL}, //node 45
+			  { 'n', NULL,           NULL}, //node 46
+			  { 0,   &node[48], &node[49]}, //node 47
+			  { 'r', NULL,           NULL}, //node 48
+			  { 0,   &node[50], &node[51]}, //node 49
+			  { 'u', NULL,           NULL}, //node 50
+			  { 'c', NULL,           NULL}, //node 51
+			  { ' ', NULL,           NULL}, //node 52
+};
+char *convert_table[] = { "00000",     //a
+					      "0001100",   //b
+					      "011111",    //c
+					      "000011",    //d
+					      "0010",      //e
+					      "010111",    //f
+					      "0001111",   //g
+					      "00110",     //h
+					      "00111",     //i
+					      "0101100011",//j
+					      "01011001",  //k
+					      "000010",    //l
+					      "010101",    //m
+					      "01101",     //n
+					      "00010",     //o
+					      "0001101",   //p
+					      "0101100001",//q
+					      "01110", 	  //r
+					      "01100",     //s
+					      "0100",      //t
+					      "011110",    //u
+					      "0101101",   //v
+					      "010100",    //w
+					      "0101100010",//x
+					      "0001110",   //y
+					      "0101100000",//z
+					      "1",         //' '
+ };
+
+
 void inflate(char *in_stream){
-
-	return;
-}
-
-void print_out_tree(Node* treeptr, char* in_stream) {
-	if (treeptr->value > 0) {
-
+	printf("\"");
+	while (*in_stream) {
+		in_stream += print_out_tree(node, in_stream);
 	}
-}
-
-void deflate(char *in_stream, char *out_stream){
-
+	printf("\"\n");
 	return;
 }
 
-void build_tree(){
-	
+int print_out_tree(Node* treeptr, char* in_stream) {
+	if (treeptr->value > 0) {
+		printf("%c", treeptr->value);
+		return 0;
+	}
+	if (*in_stream == '0') {
+		return 1 + print_out_tree(treeptr->left, in_stream+1);
+	}
+	if (*in_stream == '1') {
+		return 1 + print_out_tree(treeptr->right, in_stream+1);
+	}
+	return 0;
+}
+
+void deflate(char *in_stream){
+	while(*in_stream){
+		if(*in_stream == ' ')
+  			 printf("%s", convert_table[26]);
+
+		else printf("%s", convert_table[ (*in_stream)- 'a']);
+
+		in_stream++;
+	}
+	printf("\n");
+
 	return;
 }
 
 int main(){
+	// char in_stream[] = {"00000"
+	// 				 "0001100"
+	// 				 "011111"
+	// 				 "000011"
+	// 				 "0010"
+	// 				 "010111"
+	// 				 "0001111"
+	// 				 "00110"
+	// 				 "00111"
+	// 				 "0101100011"
+	// 				 "01011001"
+	// 				 "000010"
+	// 				 "010101"
+	// 				 "01101"
+	// 				 "00010"
+	// 				 "0001101"
+	// 				 "0101100001"
+	// 				 "01110"
+	// 				 "01100"
+	// 				 "0100"
+	// 				 "011110"
+	// 				 "0101101"
+	// 				 "010100"
+	// 				 "0101100010"
+	// 				 "0001110"
+	// 				 "0101100000"
+	// 				 "1"};
+	char in_stream[] = "0011000100000100000100001010101000001001110000010000011";
 
+	inflate(in_stream);
+	deflate("hello world my name is linda with a capital l");
 	return 0;	
 }
