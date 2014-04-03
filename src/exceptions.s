cbb27edf0f97ecba87385625fb25d5e3e388f112
Linda <llinda13579@gmail.com>
2014-04-03 13:45:48 -0400

    statted push button interrupts

--- src/exceptions.s
+++ src/exceptions.s
@@ -3,10 +3,38 @@ ISR:
 	rdctlr et, ipending
 	andi et, et, 0x100
 
-	bne et, end
+	bne et, end_isr
+
+
+	subi sp, sp, 36
+	stw ra,   0(sp)
+	stw r16,  4(sp)
+	stw r17,  8(sp)
+	stw r18, 12(sp)
+	stw r19, 16(sp)
+	stw r20, 20(sp)
+	stw r21, 24(sp)
+	stw r22, 28(sp)
+	stw r23, 32(sp)
+
+	andi r22, et, 0b10
+	beq r22, r0, end_isr
+
+	
 	
 
 
-end:
+end_isr:
+	stw ra,   0(sp)
+	stw r16,  4(sp)
+	stw r17,  8(sp)
+	stw r18, 12(sp)
+	stw r19, 16(sp)
+	stw r20, 20(sp)
+	stw r21, 24(sp)
+	stw r22, 28(sp)
+	stw r23, 32(sp)
+	addi sp, sp, 36
+
 	addi ea, ea, -4
 	eret
--- src/main.s
+++ src/main.s
@@ -7,12 +7,43 @@
 .include "deflate.s"
 .include "inflate.s"
 
+.section .data
+
+.align 2
+buttons_pushed:
+.word 0
+
 .section .text
 .global main
 main:
 
+
+movia r2, PUSH_BUTTON_ADDR
+movia r3,0xe
+stwio r3,8(r2)  /* Enable interrupts on push buttons 1,2, and 3 */
+
+movia r2,IRQ_PUSHBUTTONS
+wrctl ctl3,r2   /* Enable bit 5 - button interrupt on Processor */
+
+movia r2,1
+wrctl ctl0,r2   /* Enable global Interrupts on Processor */
+
+BUTTON_WAIT_LOOP:
+movia r8 buttons_pushed
+ldw r8, 0(r8)
+bne r8, r0, BUTTON_WAIT_LOOP
+movia r9, buttons_pushed
+stw r0, buttons_pushed
+
+andi r9, r8, 0b10
+bne r9, r0, ENCODE_DATA
+andi r9, r8, 0b100
+bne r9, r0, DECODE
+br BUTTON_WAIT_LOOP
+
 ENCODE_DATA:
 	call encode_data
+	br BUTTON_WAIT_LOOP
 
 DECODE:	
 	subi sp, sp, 4
@@ -23,8 +54,9 @@ DECODE:
 	ldw r5, 0(r5)
 
 	call decode_and_print
+	br BUTTON_WAIT_LOOP
 
 STOP:
 	br STOP;
-	
+
 .end
