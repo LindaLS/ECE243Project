39c5450122cc34392e17af06c1f3c7b4cdfaa70f
Linda <llinda13579@gmail.com>
2014-04-08 13:57:56 -0400

    added set buffer

--- src/buffer.s
+++ src/buffer.s
@@ -2,7 +2,8 @@
 # r5 src
 # r6 length in bytes - will truncate to word
 .section .text
-.global .copy_buffer
+.global copy_buffer
+.global set_buffer
 
 copy_buffer:
 	srli r6, r6, 2
@@ -22,4 +23,24 @@ COPY_BUFFER:
 
 
 FINISH_COPY_BUFFER:
+	ret
+
+# r4 src
+# r5 word to set
+# r6 length in bytes - will truncate to word
+set_buffer:
+	srli r6, r6, 2
+
+SET_BUFFER:
+	beq r6, r0, FINISH_SET_BUFFER
+
+	# copy
+	stw r5, (r4)
+
+	addi r4, r4, 4
+	addi r6, r6, -1
+
+	br SET_BUFFER	
+
+FINISH_SET_BUFFER:
 	ret
