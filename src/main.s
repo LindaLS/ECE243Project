fbf57645020dccfdb5e949f6dacabc66702d728d
Matthew Walker <walkerm930@gmail.com>
2014-04-09 13:21:17 -0400

    updated switch opcode calls, and added the read & write ones

--- src/main.s
+++ src/main.s
@@ -7,7 +7,7 @@
 .include "/home/matt/Documents/school/University/year_2 (2013)/semester_2/ECE243-Comp.Org./project/ECE243Project/src/deflate.s"
 .include "/home/matt/Documents/school/University/year_2 (2013)/semester_2/ECE243-Comp.Org./project/ECE243Project/src/inflate.s"
 .include "/home/matt/Documents/school/University/year_2 (2013)/semester_2/ECE243-Comp.Org./project/ECE243Project/src/exceptions.s"
-.include "/home/matt/Documents/school/University/year_2 (2013)/semester_2/ECE243-Comp.Org./project/ECE243Project/src/copy_buffer.s"
+.include "/home/matt/Documents/school/University/year_2 (2013)/semester_2/ECE243-Comp.Org./project/ECE243Project/src/buffer.s"
 
 
 .global main
@@ -39,60 +39,67 @@ BUTTON_WAIT_LOOP:
 	andi r9, r8, 0b111 # r9 holds opcode
 	beq r9, r0, LED_OFF
 
-	andi r10, r8, 0b1111000
+	andi r10, r8, 0xFFF8
 	srli r10, r10, 3 # 10 holds arguements
 	
 	movia r11, 0b010
-	beq r9, r11, ENCODE_DATA
+	beq r9, r11, DECODE_DATA
 	movia r11, 0b011
-	beq r9, r11, DECODE
+	beq r9, r11, ENCODE_DATA
 	movia r11, 0b101
 	beq r9, r11, READ_SD
 	movia r11, 0b100
 	beq r9, r11, WRITE_SD
-	# br DECODE
 
 br BUTTON_WAIT_LOOP
 
 
 ENCODE_DATA:
+	andi r10, r10, 0b1
 	movia r4, TEST_UNCOMPRESSED_DATA
-	movia r5, FILE_IN_BUFFER
+	bne r10, r0, actually_encode # if not 0, keep r4 as TEST_UNCOMPRESSED_DATA
+	movia r4, SD_ADDR # if 0 set it to the SD card core's buffer
+
+actually_encode:
+	movia r5, BLOCK_BUFFER
 	call encode_data
 	br LED_ON
 
-DECODE:	
-	subi sp, sp, 4
-	stw ra, 0(sp)
-
-	# movia r4, FILE_IN_NAME
-	# movia r5, FILE_IN_BUFFER
-	# movia r6, FILE_IN_BUFFER_LENGTH
-	# ldw r6, 0(r6)
-	# call readFile
-
-	# movia r4, FILE_IN_NAME
-	# movia r5, FILE_IN_BUFFER_LENGTH
-	
+DECODE_DATA:
+	andi r10, r10, 0b1
 	movia r4, TEST_COMPRESSED_DATA
-	movia r5, TEST_COMPRESSED_DATA_LENGTH
-	ldw r5, 0(r5)
-	movia r6, FILE_IN_BUFFER
-
-	call decode_and_print
+	bne r10, r0, actually_decode # if 0 keep them at TEST_COMPRESSED_DATA
+	movia r4, SD_ADDR
+	
+actually_decode:
+	movia r6, BLOCK_BUFFER
+	call decode_and_print_getlengthfirst
 	br LED_ON
 
 READ_SD:
-
-	movia r4, 0
+	andi r4, r10, 0b11
 	movia r5, SD_ADDR
 	call read_file
 
-	movia r4, FILE_IN_BUFFER
+	movia r4, BLOCK_BUFFER
 	movia r5, SD_ADDR
-	movia r6, 512
+	movia r6, BLOCK_BUFFER_LENGTH
+	ldw r6, 0(r6)
 	call copy_buffer
 	br LED_ON
+
+WRITE_SD:
+	movia r4, SD_ADDR
+	movia r5, BLOCK_BUFFER
+	movia r6, BLOCK_BUFFER_LENGTH
+	call copy_buffer
+
+	andi r4, r10, 0b11
+	movia r5, SD_ADDR
+	call write_file
+
+	br LED_ON
+	
 
 LED_OFF:
 	movia r8, ADDR_GREENLEDS
