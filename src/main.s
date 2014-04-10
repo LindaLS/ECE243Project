af4d20fd2eeb513f469d49a8bcc67781216748bf
Matthew Walker <walkerm930@gmail.com>
2014-04-09 23:36:53 -0400

    re-did opcodes and arguments, added new calls to print functions

--- src/main.s
+++ src/main.s
@@ -8,6 +8,7 @@
 .include "/home/matt/Documents/school/University/year_2 (2013)/semester_2/ECE243-Comp.Org./project/ECE243Project/src/inflate.s"
 .include "/home/matt/Documents/school/University/year_2 (2013)/semester_2/ECE243-Comp.Org./project/ECE243Project/src/exceptions.s"
 .include "/home/matt/Documents/school/University/year_2 (2013)/semester_2/ECE243-Comp.Org./project/ECE243Project/src/buffer.s"
+.include "/home/matt/Documents/school/University/year_2 (2013)/semester_2/ECE243-Comp.Org./project/ECE243Project/src/strings.s"
 
 
 .global main
@@ -48,25 +49,27 @@ BUTTON_WAIT_LOOP:
 
 
 	andi r9, r8, 0b111 # r9 holds opcode
-	beq r9, r0, LED_OFF
-
-	andi r10, r8, 0xFFF8
-	srli r10, r10, 3 # 10 holds arguements
+	srli r10, r8, 3 # 10 holds arguments
 	
+	beq r9, r0, LED_OFF
+	movia r11, 0b001
+	beq r9, r11, DECODE_OR_ENCODE
 	movia r11, 0b010
-	beq r9, r11, DECODE_DATA
+	beq r9, r11, READ_OR_WRITE
 	movia r11, 0b011
-	beq r9, r11, ENCODE_DATA
-	movia r11, 0b101
-	beq r9, r11, READ_SD
-	movia r11, 0b100
-	beq r9, r11, WRITE_SD
+	beq r9, r11, PRINT_OUR_BUFFER
+	movia r11, 0b111
+	beq r9, r11, CLEAR_BUFFER
 
 br BUTTON_WAIT_LOOP
 
+DECODE_OR_ENCODE:
+	andi r11, r10, 0b1 # get 1st arg
+	beq r11, r0, DECODE_DATA # if 0, decode
+	br ENCODE_DATA # else encode
 
 ENCODE_DATA:
-	andi r10, r10, 0b1
+	andi r10, r10, 0b10 # get 2nd arg
 	movia r4, TEST_UNCOMPRESSED_DATA
 	bne r10, r0, actually_encode # if not 0, keep r4 as TEST_UNCOMPRESSED_DATA
 	movia r4, SD_ADDR # if 0 set it to the SD card core's buffer
@@ -77,7 +80,7 @@ actually_encode:
 	br LED_ON
 
 DECODE_DATA:
-	andi r10, r10, 0b1
+	andi r10, r10, 0b10 # get 2nd arg
 	movia r4, TEST_COMPRESSED_DATA
 	bne r10, r0, actually_decode # if 0 keep them at TEST_COMPRESSED_DATA
 	movia r4, SD_ADDR
@@ -87,8 +90,14 @@ actually_decode:
 	call decode_and_print_getlengthfirst
 	br LED_ON
 
+READ_OR_WRITE:
+	andi r10, r10, 0b1
+	beq r10, r0, READ_SD # if 0 read
+	br WRITE_SD # else write
+
 READ_SD:
-	andi r4, r10, 0b11
+	andi r4, r10, 0b110 # mask out 2nd and 3rd args
+	srli r4, r4, 1 # shift the bits down
 	movia r5, SD_ADDR
 	call read_file
 
@@ -107,7 +116,8 @@ WRITE_SD:
 	
 	call copy_buffer
 
-	andi r4, r10, 0b11
+	andi r4, r10, 0b110 # mask out 2nd and 3rd args
+	srli r4, r4, 1 # shift the bits down
 	movia r5, SD_ADDR
 	call write_file
 
@@ -122,12 +132,19 @@ CLEAR_BUFFER:
 	call set_buffer
 	br LED_ON
 
+PRINT_OUR_BUFFER:
+	andi r11, r10, 0b1 # get arg 1
+	beq r11, r0, PRINT_ENCODED_BUFFER # if arg is 0, print encoded
+	br PRINT_DECODED_BUFFER # else print as decoded
+
 PRINT_ENCODED_BUFFER:
 	movia r4, BLOCK_BUFFER
-	ldb r5, 0(r4)
-	ldb r6, 0(r4)
+	ldbu r5, 0(r4)
+	ldbu r6, 1(r4)
 	slli r5, r5, 4
 	or r5, r5, r6
+	srli r5, r5, 3 # divide by 8 - was # bits
+	addi r5, r5, 1 # will usually round down
 	addi r4, r4, 2
 
 	call print_string
